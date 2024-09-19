6ce1095fba367c4e035dc6e4a261f4981e7065cf
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-09-19 15:23:10 +0300

    added nroute

--- API/views.py
+++ API/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Category, Subcategory, Brand, Model, Item, Stock
 from .serializers import (
     CategorySerializer, SubcategorySerializer, BrandSerializer, ModelSerializer,
@@ -11,11 +14,25 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    @action(detail=True, methods=['get'])
+    def subcategories(self, request, pk=None):
+        category = self.get_object()
+        subcategories = category.subcategories.all()
+        serializer = SubcategorySerializer(subcategories, many=True)
+        return Response(serializer.data)
+
 
 # Subcategory ViewSet
 class SubcategoryViewSet(viewsets.ModelViewSet):
     queryset = Subcategory.objects.all()
     serializer_class = SubcategorySerializer
+
+    @action(detail=True, methods=['get'])
+    def items(self, request, pk=None):
+        subcategory = self.get_object()
+        items = subcategory.items.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
 
 
 # Brand ViewSet
@@ -23,11 +40,25 @@ class BrandViewSet(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
 
+    @action(detail=True, methods=['get'])
+    def models(self, request, pk=None):
+        brand = self.get_object()
+        models = brand.models.all()
+        serializer = ModelSerializer(models, many=True)
+        return Response(serializer.data)
+
 
 # Model ViewSet
 class ModelViewSet(viewsets.ModelViewSet):
     queryset = Model.objects.all()
     serializer_class = ModelSerializer
+
+    @action(detail=True, methods=['get'])
+    def items(self, request, pk=None):
+        model = self.get_object()
+        items = model.items.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
 
 
 # Item ViewSet
@@ -35,11 +66,25 @@ class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
+    @action(detail=True, methods=['get'])
+    def stocks(self, request, pk=None):
+        item = self.get_object()
+        stocks = item.stocks.all()
+        serializer = StockSerializer(stocks, many=True)
+        return Response(serializer.data)
+
 
 # Stock ViewSet
 class StockViewSet(viewsets.ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+
+    @action(detail=True, methods=['get'])
+    def item(self, request, pk=None):
+        stock = self.get_object()
+        item = stock.item
+        serializer = ItemSerializer(item)
+        return Response(serializer.data)
 
 
 # Stock Availability View (Retrieve)
@@ -47,3 +92,5 @@ class StockAvailabilityView(viewsets.ReadOnlyModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockAvailabilitySerializer
     lookup_field = 'item_id'
+
+

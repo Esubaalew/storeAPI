dba128c460203de8bd2496b815b72100012eccf8
esubaalew <esubalewchekol6@gmail.com>
2024-10-01 09:21:39 +0300

    register models to admin site

--- API/admin.py
+++ API/admin.py
@@ -1,9 +1,46 @@
 from django.contrib import admin
-from .models import Category, Subcategory, Brand, Model, Item, Stock
+from .models import Category, Subcategory, Brand, Model, Item, Stock, Request, Message
 
-admin.site.register(Category)
-admin.site.register(Subcategory)
-admin.site.register(Brand)
-admin.site.register(Model)
-admin.site.register(Item)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+    list_per_page = 10
+
+
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    search_fields = ('name', 'category__name')
+    ordering = ('category', 'name')
+    list_per_page = 10
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subcategory')
+    search_fields = ('name', 'subcategory__name')
+    ordering = ('subcategory', 'name')
+    list_per_page = 10
+
+
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'subcategory')
+    search_fields = ('name', 'brand__name', 'subcategory__name')
+    ordering = ('brand', 'subcategory', 'name')
+    list_per_page = 10
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Subcategory, SubcategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Model, ModelAdmin)
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Stock)
+admin.site.register(Request)
+
+# changing the titles
+admin.site.site_title = 'Milka'
+admin.site.site_header = 'Milka'
